bae837fdca1a1d2c09399b810e311cf7d229f113
NickSugar <nicksugar1@outlook.com>
2018-06-26 19:30:48 -0700

    Styled input on known calorie form and added .sass-cache to gitignore

--- public/router.js
+++ public/router.js
@@ -3,27 +3,26 @@ import { app } from './app.js'
 
 var init = function () {
 	
-    if (window.location.pathname == ('/' || '/landing')) {
-        app.helperFunctions.loadComponent(app.controllers.landingCtrl)
-    }
+	if (window.location.pathname == ('/' || '/landing')) {
+	app.helperFunctions.loadComponent(app.controllers.landingCtrl)
+	}
 
-    var header = document.getElementById('headerContainer')
-    if (header) {
-        app.helperFunctions.loadComponent(app.controllers.headerCtrl, 'headerContainer')
-    }
+	var header = document.getElementById('headerContainer')
+	if (header) {
+	app.helperFunctions.loadComponent(app.controllers.headerCtrl, 'headerContainer')
+	}
 
-    var footer = document.getElementById('footerContainer')
-    if (footer) {
-        app.helperFunctions.loadComponent(app.controllers.footerCtrl, 'footerContainer')
-    }
+	var footer = document.getElementById('footerContainer')
+	if (footer) {
+	app.helperFunctions.loadComponent(app.controllers.footerCtrl, 'footerContainer')
+	}
 
 	var components = app.controllers
 	for (var component in components){
-		console.log('test loop')
-		var componentPattern = new RegExp(components.component.name, 'i','g')
+		var componentPattern = new RegExp(components[component].name, 'i','g')
 		var pathname = window.location.pathname 
 		if (componentPattern.test(pathname)) {
-        		app.helperFunctions.loadComponent(components.component)
+			app.helperFunctions.loadComponent(components[component])
 		} 	
 	}
 }
